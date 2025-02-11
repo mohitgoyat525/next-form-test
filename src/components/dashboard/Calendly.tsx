67b01df5a9b83cf03d63e49f2777e1ccdab110c4
mohitgoyat525 <montyits71@gmail.com>
2025-02-11 16:21:07 +0530

    add-changes

--- src/components/dashboard/Calendly.tsx
+++ src/components/dashboard/Calendly.tsx
@@ -1,28 +1,13 @@
 "use client";
-import { useState, useEffect } from "react";
+import { InlineWidget } from "react-calendly";
 
 const Calenderly = () => {
-  const [search, setSearch] = useState("");
-
-  useEffect(() => {
-    setSearch(new URLSearchParams(window.location.search).get("search") || "");
-    const script = document.createElement("script");
-    script.src = "https://assets.calendly.com/assets/external/widget.js";
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h2 className="text-3xl font-bold text-gray-800 mb-4">Calenderly</h2>
-      <div
-        className="calendly-inline-widget w-full sm:w-[500px] h-[650px] border rounded-lg shadow-lg max-lg:max-w-2xl"
-        data-url="https://calendly.com/montyits71/new-meeting?text_color=ffffff&primary_color=140202"
-      ></div>
+    <div className="flex flex-col items-center justify-center bg-gray-100 py-20">
+      <h2 className="text-3xl font-bold text-gray-800 mb-4">Calendly</h2>
+      <div className="sm:w-[500px] h-[650px]">
+        <InlineWidget url="https://calendly.com/montyits71/new-meeting" />
+      </div>
     </div>
   );
 };
